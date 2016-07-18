1d6647981c2e94c3935e49347dde8c39a33e6a95
Elijah Bernstein-Cooper <elijah.bc@omniresources.com>
2016-07-18 09:01:12 -0500

    loads debugging dataset in DB via MLCP
    + 20 docs in example dataset

---
+++ scripts/loadSmallData.sh
@@ -0,0 +1,19 @@
+
+
+# if on windows git bash, replace with Windows filepath
+export file=$( readlink -f ../data/police-calls-small.xml )
+export file="${file/\/c\//\/C:/}"
+echo Loading $file...
+echo 
+
+# import data with content pump
+mlcp.sh import \
+-host localhost \
+-port 7004 \
+-username admin \
+-password admin \
+-mode local \
+-input_file_path $file \
+-input_file_type aggregates \
+-aggregate_record_element row \
+-output_uri_replace "$file,'/calls/'"
