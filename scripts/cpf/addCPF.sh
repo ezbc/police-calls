360612ad512ae6980c7631aae5f29628886b1c7d
Elijah Bernstein-Cooper <elijah.bc@omniresources.com>
2016-07-19 14:26:05 -0500

    excess scripts which perform like createCPF.sh

--- scripts/cpf/addCPF.sh
+++
@@ -1,2 +0,0 @@
-curl -X POST  --anyauth --user admin:admin --header "Content-Type:application/json" \
--d@setup-cpf.json http://localhost:8002/manage/v2/databases/police-calls-DB/cpf-configs?format=json
--- scripts/cpf/addPipeline.sh
+++
@@ -1,3 +0,0 @@
-curl -X POST --anyauth --user admin:admin \
---header "Content-Type:application/xml" -d@pipeline-options.xml \
-http://localhost:8002/manage/v2/databases/police-calls-Triggers/pipelines?format=json
