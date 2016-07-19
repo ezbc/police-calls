aa9df883b60ba1a008dc6500d321af221909ecb6
Elijah Bernstein-Cooper <elijah.bc@omniresources.com>
2016-07-19 14:17:41 -0500

    Deletes trigger database associated with the CPF

---
+++ scripts/cpf/deleteCPF.sh
@@ -0,0 +1,11 @@
+
+# Change the triggers DB so that police-calls-triggers can be deleted
+curl -X PUT --digest -u admin:admin -H "Content-type: application/xml" \
+-d '<database-properties><triggers-database>Triggers</triggers-database></database-properties>'  \
+http://localhost:8002/manage/v2/databases/police-calls-DB/properties
+
+# Remove the triggers DB
+curl -X DELETE --anyauth -u admin:admin --header "Content-Type:application/json" \
+http://localhost:8002/manage/v2/databases/police-calls-Triggers
+
+
