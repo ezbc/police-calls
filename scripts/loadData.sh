922d3fcd54e84ff8f127f4030a196b193023355d
Elijah Bernstein-Cooper <elijah.bc@omniresources.com>
2016-07-25 10:26:27 -0500

    Adopted UUID assignment from loadSmallData.sh

--- scripts/loadData.sh
+++ scripts/loadData.sh
@@ -7,6 +7,8 @@ echo Loading $file...
 echo 
 
 # import data with content pump
+# regex ^ replaces the first character (empty) of the string
+# @_uuid uses the element attribute _uuid to assign the URI
 mlcp.sh import \
 -host localhost \
 -port 7004 \
@@ -16,4 +18,5 @@ mlcp.sh import \
 -input_file_path $file \
 -input_file_type aggregates \
 -aggregate_record_element row \
--output_uri_replace "$file,'/calls/'"
+-output_uri_replace "^,'/calls/call/'" \
+-uri_id @_uuid
